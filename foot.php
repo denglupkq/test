11cad2918c69eb7f55b74827c39c81d95c742c9e
denglupkq <51144979+denglupkq@users.noreply.github.com>
2019-05-29 11:52:16 +0800

    你好
    我在修改你的项目

--- foot.php
+++ foot.php
@@ -10,7 +10,7 @@ include("cery.php");
 	<body>
 		<div class="foot">
 			<ul>
-				<li><center>版权所有:天马工作室</center></li>
+				<li><center>版权所有:天wwdw马工作室</center></li>
 				<li><center>电话:5418894188</center></li>
 				<li><center>邮箱:54188@qq.com</center></li>
 				<li><center>不要问我为什么这么赚钱,因为我们质量好</center></li>
